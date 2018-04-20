2896c7f591b625bd07d82befe36d7b3d6ef30a2a
chiukenny <chiu.kenny@hotmail.com>
2018-04-19 21:12:27 -0700

    Vector quant test code. Test image in dropbox.

---
+++ Sample Codes/test_code.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import numpy
+from scipy.cluster.vq import kmeans, vq
+
+# load image
+im = Image.open("test.tif")
+# im.show()
+
+# convert image into ndarray
+imarray = numpy.array(im)
+
+# dat = numpy.reshape(imarray, (7709*6001, 4))
+
+# work with subimage
+# 
+n = 100
+dat = imarray[0:n, 0:n,]
+
+# display subimage
+im2 = Image.fromarray(dat)
+im2.show()
+
+# reshape into 2d-array
+dat2 = numpy.reshape(dat, (n*n, 4))
+
+# convert into float
+dat3 = dat2 / 255
+
+# kmeans -- returns list of means ("codebook")
+codebook, distortion = kmeans(dat3, 3, 10)
+
+# vector quant -- returns index of closest mean for each pixel ("code")
+code, dist = vq(dat3, codebook)
+
+# replace list of index with mean
+dat4 = numpy.asarray([codebook[i] for i in code])
+
+# convert back into uint8 and reshape back into subimage
+dat4 = dat4 * 255
+dat4 = dat4.round()
+dat4 = numpy.uint8(dat4)
+dat5 = numpy.reshape(dat4, (n,n,4))
+
+# display vector quant image
+res = Image.fromarray(dat5)
+res.show()
