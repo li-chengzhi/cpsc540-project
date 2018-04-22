2506145cd19a957444fb136c7e9f6a0b404bde04
chiukenny <chiu.kenny@hotmail.com>
2018-04-22 15:30:56 -0700

    Label prop w/ negative label. Not working?

--- Sample Codes/test_label_propagation.py
+++ Sample Codes/test_label_propagation.py
@@ -10,11 +10,12 @@ im = Image.open("clouds.tif")
 imarray = numpy.array(im)
 
 # work with subimage
-n = 2000 # roughly 1/7 of height
+n = 2000 # roughly 1/7 of original height
 width = imarray.shape[1]
-dat = imarray[0:n,width-n:width,:]
+dat = imarray[0:n,width-n:width,0:3]
 
 # http://nicodjimenez.github.io/boxLabel/annotate.html
+# get manually labeled cloud pixels
 file_labels = ""
 with open("clouds.txt","r") as f:
     for line in f:
@@ -30,8 +31,8 @@ while True:
     str_bounds = str_labels[beg+6:end]
     str_labels = str_labels[end+7:]
     bounds = [int(i) for i in str_bounds.split()]
-    for x in range(bounds[1], bounds[3]):
-        for y in range(bounds[0], bounds[2]):
+    for x in range(bounds[1], bounds[3]+1):
+        for y in range(bounds[0], bounds[2]+1):
             labeled.append([x,y])
 
 labeled = numpy.asarray(labeled)
@@ -51,28 +52,78 @@ labeled_2d = skimage.block_reduce(labeled_2d, (s,s), numpy.max)
 lab_samp_reduced = list(zip(*numpy.where(labeled_2d==1)))
 n = int(numpy.floor(n/s))
 
-dat_labeled = dat
+# display sampled labeled pixels
+dat_labeled = numpy.copy(dat)
 
 def colour_labeled(pixel):
-    dat_labeled[pixel[0],pixel[1]] = [255,192,203,0]
+    dat_labeled[pixel[0],pixel[1]] = [255,0,0]
     
 numpy.apply_along_axis(colour_labeled, 1, lab_samp_reduced)
 
 im2 = Image.fromarray(dat_labeled)
 im2.show()
 
-X = numpy.reshape(dat, (n*n,4))
+
+# label propagation inputs
+X = numpy.reshape(dat, (n*n,3))
 y = numpy.reshape(labeled_2d, (n*n))
 
-label_prop = label_propagation.LabelPropagation(kernel='knn')
+# set -1 for unlabeled
+y[y == 0] = -1
+
+# set weight based on RGB values
+def colour_threshold(x):
+#    return numpy.linalg.norm(x) < 50
+    return numpy.exp(-numpy.linalg.norm(x))
+#    return 1
+
+# convert (x,y) index into a flattened 1d array index
+def get_flattened_index(x,y,n1,n2):
+    i, = numpy.unravel_index(numpy.ravel_multi_index((x,y), (n1,n1)), n2)
+    return i
+
+# include a negative label...
+y[get_flattened_index(n-1,0,n,n*n)] = 0
+
+# custom kernel for label propagation
+# only 8 pixel neighbours may be non-zero
+def custom_kernel(X1, X2):
+    n1, d1 = X1.shape
+    n = int(numpy.sqrt(n1))
+    W = numpy.zeros((n1,n1))
+    for i in range(n1):
+        x,y = numpy.unravel_index(i, (n,n))
+        W[i,i] = 1
+        if y < n-1:
+            ii = get_flattened_index(x,y+1,n,n1)
+            W[i,ii] = W[ii,i] = colour_threshold(X1[i,:]-X2[ii,:])
+            if x > 0:
+                ii = get_flattened_index(x-1,y+1,n,n1)
+                W[i,ii] = W[ii,i] = colour_threshold(X1[i,:]-X2[ii,:])
+            if x < n-1:
+                ii = get_flattened_index(x+1,y+1,n,n1)
+                W[i,ii] = W[ii,i] = colour_threshold(X1[i,:]-X2[ii,:])
+        if x < n-1:
+            ii = get_flattened_index(x+1,y,n,n1)
+            W[i,ii] = W[ii,i] = colour_threshold(X1[i,:]-X2[ii,:])
+            if y > 0:
+                ii = get_flattened_index(x+1,y-1,n,n1)
+                W[i,ii] = W[ii,i] = colour_threshold(X1[i,:]-X2[ii,:])
+    return W
+#    return -numpy.ones((X1.shape[0], X1.shape[0]))
+
+label_prop = label_propagation.LabelPropagation(custom_kernel, max_iter=50, n_jobs=-1)
 label_prop.fit(X,y)
 
 trans_labels = label_prop.transduction_
-trans_lab_2d = numpy.asarray(trans_labels)
-trans_lab_2d = numpy.reshape(trans_lab_2d, (n,n))
+trans_labels = numpy.asarray(trans_labels)
+#trans_lab_2d = numpy.reshape(trans_lab_2d, (n,n))
 
-#dat_labeled = dat
-#numpy.apply_along_axis(colour_labeled, 1, lab_samp_reduced)
-#
-#im2 = Image.fromarray(dat_labeled)
-#im2.show()
+trans_labs = [numpy.unravel_index(i, (n,n)) for i, x in enumerate(trans_labels) if x == 1]
+
+# display resulting labels
+dat_labeled = numpy.copy(dat)
+numpy.apply_along_axis(colour_labeled, 1, trans_labs)
+
+im2 = Image.fromarray(dat_labeled)
+im2.show()
