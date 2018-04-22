788eec6ff82bc5ea5db66496cf478b00bf204973
chiukenny <chiu.kenny@hotmail.com>
2018-04-21 18:39:50 -0700

    Prepare data for cloud test.

--- Sample Codes/test_clouds.py
+++ Sample Codes/test_clouds.py
@@ -40,10 +40,49 @@ labeled = numpy.asarray(labeled)
 labeled_sample = numpy.random.choice(labeled.shape[0], int(numpy.floor(labeled.shape[0]/4)), replace=False)
 labeled_sample = labeled[labeled_sample,:]
 
+dat_labeled = dat
+
 def colour_labeled(pixel):
-    dat[pixel[0],pixel[1]] = [255,192,203,0]
+    dat_labeled[pixel[0],pixel[1]] = [255,192,203,0]
     
 numpy.apply_along_axis(colour_labeled, 1, labeled_sample)
 
-im2 = Image.fromarray(dat)
+im2 = Image.fromarray(dat_labeled)
 im2.show()
+
+X = []
+for x in range(1,n-2):
+    for y in range(1,n-2):
+        # features:
+        # (x, y, <pixel values>, <8 neighbour pixel values>)
+        row = [x,y]
+        row += dat[x,y,:].tolist()
+        row += dat[x-1,y-1,:].tolist()
+        row += dat[x,y-1,:].tolist()
+        row += dat[x+1,y-1,:].tolist()
+        row += dat[x-1,y,:].tolist()
+        row += dat[x+1,y,:].tolist()
+        row += dat[x-1,y+1,:].tolist()
+        row += dat[x,y+1,:].tolist()
+        row += dat[x+1,y+1,:].tolist()
+        X.append(row)
+X = numpy.asarray(X)
+Y = numpy.zeros((n,n))
+
+def set_label(pixel):
+    Y[pixel[0],pixel[1]] = 1
+
+numpy.apply_along_axis(set_label, 1, labeled_sample)
+Y = Y[1:n-2,1:n-2]
+Y = numpy.reshape(Y,((n-3)*(n-3),1))
+
+# https://github.com/phuijse/bagging_pu/blob/master/PU_Learning_simple_example.ipynb
+from sklearn.tree import DecisionTreeClassifier
+
+NP = X.shape[0]
+NU = n*n - NP
+
+T = 10
+K = NP
+train_label = numpy.zeros(NP+K)
+train_label[:NP] = 1.0
